0446abb50162545cf01de1c3acd251b0643e0579
Ovinda_Vidushan <ovindavidushan22@gmail.com>
2026-05-12 09:09:33 +0530

    Final Out put with comments

--- Word-Guessing-Game/Word_Guessing_Game.py
+++ Word-Guessing-Game/Word_Guessing_Game.py
@@ -10,5 +10,31 @@ word=random.choice(words) #selecets a random word from the words list
 
 print("Guess the characters") #promting useer to guess
 
-guesses="" 
+guesses=""  #initializing guessed and turns
 turns=12
+
+while turns > 0:     #checking each character in the word
+    failed = 0
+    for char in word:
+            if char in guesses:
+                print(char, end="")
+            else:
+                print("_")
+                failed+=1
+    if failed == 0:                     #checking if user has won
+        print("You Win")
+        print("The word is:",word)
+        break
+    print()
+    guess = input("guess the character:") #promting for the next guess
+    
+    guesses += guess
+
+    if guess not in word:               #handling the incorrect guesses
+        turns -= 1
+        print("Wrong")
+        print("You Have",+turns,"more guesses")
+    
+    if turns == 0:                       #checking if the user has lost
+        print("You Loose")
+                
